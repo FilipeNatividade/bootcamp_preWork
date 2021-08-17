b49d96d16747b85c6b3f8d64174ec5e3cda3e75d
Filipe <n_filipe_n@hotmail.com>
2021-08-17 13:35:48 -0300

    day1/finished

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,35 @@
-import './style.css'
+import "./style.css";
 
-const app = document.querySelector('[data-js="app"]').innerHTML = `
+let show = true;
+
+const app = document.querySelector('[data-js="app"]');
+app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
-`
+`;
+
+const anchor = document.querySelector('[data-js="anchor"]');
+anchor.innerHTML = 'Não mostrar APP';
+
+anchor.addEventListener(
+  "click",
+  (event) => {
+    event.preventDefault();
+
+    if (show === true) {
+      show = false;
+      app.style.visibility = "hidden";
+      anchor.innerHTML = 'Mostrar APP';
+    } else {
+      show = true;
+      app.style.visibility = "visible";
+      anchor.innerHTML = 'Não mostrar APP';
+    }
+
+  },
+  false
+);
+
+
+
+
