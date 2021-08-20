c914253c9564afcc6fd3f65599cc45e5a4c932e2
Filipe <n_filipe_n@hotmail.com>
2021-08-20 03:07:53 -0300

    exercise3/completed

---
+++ dia3/src/cars.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('[data-js="formCars"]')
+const tableHead = document.querySelector('[data-js="tableHead"]')
+
+form.addEventListener('submit', e =>{
+  e.preventDefault()
+
+  const image = e.target.elements.image
+  const brand = e.target.elements.brand
+  const year = e.target.elements.year
+  const plate = e.target.elements.plate
+  const color = e.target.elements.color
+
+  const inputs = [image, brand, year, plate, color]
+
+  const tableRow = document.createElement('tr')
+  inputs.forEach(item => {
+    const tableData = document.createElement('td')
+      tableData.innerText = item.value
+      tableRow.appendChild(tableData)
+  })
+
+  tableHead.appendChild(tableRow)
+  e.target.reset()
+  image.focus()
+})
