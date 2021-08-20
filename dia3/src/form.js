8d63d443e1dc318ba8940ccc5bbd10acc773ecf6
Filipe <n_filipe_n@hotmail.com>
2021-08-20 02:01:33 -0300

    exercise2/completed

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -2,6 +2,13 @@ const inputName = document.querySelector('[data-js="inputName"]');
 const paragraph = document.querySelector('[data-js="paragraph"]');
 
 const noCapitals = ["da", "das", "de", "do", "dos"];
+const colors = [
+  { name: "red", value: "#f00" },
+  { name: "green", value: " #008000" },
+  { name: "azul", value: " #87ceeb" },
+  { name: "laranja", value: "#ffa500" },
+  { name: "rosa", value: "#ff007f" },
+];
 
 paragraph.innerText = `Resultado: `;
 
@@ -18,3 +25,42 @@ inputName.addEventListener("input", (e) => {
 
   paragraph.innerText = `Resultado: ${name}`;
 });
+
+const containerFormSel = document.createElement("div");
+containerFormSel.className = "container";
+
+const titleForm2 = document.createElement("h2");
+titleForm2.innerText = "Exercício 2";
+containerFormSel.appendChild(titleForm2);
+
+const form = document.createElement("form");
+containerFormSel.appendChild(form);
+
+const select = document.createElement("select");
+select.setAttribute("multiple", "");
+form.appendChild(select);
+
+colors.forEach((color) => {
+  const optionColor = document.createElement("option");
+  optionColor.style.color = color.value
+  optionColor.textContent = color.name;
+  optionColor.value = color.value;
+  select.appendChild(optionColor);
+});
+
+const subContainer = document.createElement("div");
+subContainer.className = "subContainerColor";
+containerFormSel.appendChild(subContainer);
+
+select.addEventListener("change", (e) => {
+  subContainer.innerHTML = ''
+  Array.from(e.target.selectedOptions).forEach((option) => {
+    const showColor = document.createElement("div");
+    showColor.className = 'showColor'
+    showColor.style.backgroundColor = option.value
+    subContainer.appendChild(showColor)
+  });
+
+});
+
+document.body.appendChild(containerFormSel);
